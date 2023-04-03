588e0da40a43d83117b4bd58266794a260319d0b
Максим Шашков <88987688+Maks-Shashkov@users.noreply.github.com>
2023-04-03 15:17:29 +0300

    Update EquationController.h

--- Application/EquationController.h
+++ Application/EquationController.h
@@ -1,17 +1,21 @@
-//#pragma once
-//class EquationController
-//{
-//private:
-//    int A, B, C;
-//public:
-//    int Extraction_a(const std::string& equation);
-//
-//    string remove_x_squared(string equation);
-//
-//    void removeSpaces(std::string& str);
-//
-//    int Extraction_b(const std::string& equation);
-//
-//    int Extraction_c(const std::string& equation);
-//};
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class EquationController
+{
+public:
+    int Extraction_a(const std::string& equation);
+
+    string remove_x_squared(string equation);
+
+    void removeSpaces(std::string& str);
+
+    int Extraction_b(const std::string& equation);
+
+    int Extraction_c(const std::string& equation);
+};
 
